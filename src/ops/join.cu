0d9ef0abd434805d24b5e6f2e6a42e6847af0822
Ronak kalvani <97736833+ronakkalvani@users.noreply.github.com>
2024-06-03 13:46:30 +0800

    Update join.cu

--- src/ops/join.cu
+++ src/ops/join.cu
@@ -7,8 +7,8 @@
 
 #include <cuda.h>
 #include <cub/util_allocator.cuh>
-#include "cub/test/test_util.h"
-
+//#include "cub/test/test_util.h"
+#include "/csproject/yike/intern/ronak/crystal/cccl/cub/test/test_util.h"
 #include "crystal/crystal.cuh"
 
 #include "utils/generator.h"
